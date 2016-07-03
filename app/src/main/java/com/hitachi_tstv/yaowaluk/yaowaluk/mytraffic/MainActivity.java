665b71b255f151a51f3c7dfdaca517a3e5cf8bfd
yaowamuz <yaowamuz.c@gmail.com>
2016-07-03 13:04:43 +0700

    Design UI  Detail finish

--- app/src/main/java/com/hitachi_tstv/yaowaluk/yaowaluk/mytraffic/MainActivity.java
+++ app/src/main/java/com/hitachi_tstv/yaowaluk/yaowaluk/mytraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -18,26 +19,25 @@ public class MainActivity extends AppCompatActivity {
     private Button aboutMeButton;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    //Bind Widget : การผูกความสัมพันธ์ตัวWidget บน Activity
+        //Bind Widget : การผูกความสัมพันธ์ตัวWidget บน Activity
         trafficListView = (ListView) findViewById(R.id.listView);
         aboutMeButton = (Button) findViewById(R.id.button);
 
         //Cast to :: Alt + Enter
         //key end :: Shift + Ctrl +Enter
-    //------------end Bind Widget---------------
+        //------------end Bind Widget---------------
 
         // Controller for button
         aboutMeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Sound Effect
-                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.bird);
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.bird);
                 mediaPlayer.start();
 
 
@@ -52,8 +52,59 @@ public class MainActivity extends AppCompatActivity {
 
         // ----end----Controller for button
 
-    }// Main Method and statement in method
+        //--Create Listview
+        //1.Setter Array Simple
+        final int[] iconInts = new int[20];
+        iconInts[0] = R.drawable.traffic_01;
+        iconInts[1] = R.drawable.traffic_02;
+        iconInts[2] = R.drawable.traffic_03;
+        iconInts[3] = R.drawable.traffic_04;
+        iconInts[4] = R.drawable.traffic_05;
+        iconInts[5] = R.drawable.traffic_06;
+        iconInts[6] = R.drawable.traffic_07;
+        iconInts[7] = R.drawable.traffic_08;
+        iconInts[8] = R.drawable.traffic_09;
+        iconInts[9] = R.drawable.traffic_10;
+        iconInts[10] = R.drawable.traffic_11;
+        iconInts[11] = R.drawable.traffic_12;
+        iconInts[12] = R.drawable.traffic_13;
+        iconInts[13] = R.drawable.traffic_14;
+        iconInts[14] = R.drawable.traffic_15;
+        iconInts[15] = R.drawable.traffic_16;
+        iconInts[16] = R.drawable.traffic_17;
+        iconInts[17] = R.drawable.traffic_18;
+        iconInts[18] = R.drawable.traffic_19;
+        iconInts[19] = R.drawable.traffic_20;
 
+        // 2. Call From xml
+        final String[] nameString = getResources().getStringArray(R.array.name);
+
+        // 3. Call java class
+        MyData myData = new MyData(); // Create object or instance
+        String[] detStrings = myData.getDetailStrings();
+
+
+        //Inheriate to MyAdapter
+        MyAdapter myAdapter = new MyAdapter(detStrings,nameString,iconInts,this);
+        trafficListView.setAdapter(myAdapter);
+
+
+        //Intent to Detail When Click
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+                intent.putExtra("Name", nameString[position]);
+                intent.putExtra("Icon", iconInts[position]);
+                intent.putExtra("Index", position);
+                startActivity(intent);
+
+            }//On ItemClick
+
+
+        });
+
+    }// Main Method and statement in method
 
 
 }// Main Class นี่คือคลาสหลัก
